6ccae1f9b9423c9729e435633b49e8e2cf074495
sikandarimran01 <87462286+sikandarimran01@users.noreply.github.com>
2025-07-01 10:30:38 +0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'sikandarimran01@gmail.com'
-app.config['MAIL_PASSWORD'] = 'your_app_password'  # <-- Use env var in production
+app.config['MAIL_PASSWORD'] = 'wize wwiv ybdb urmk'  # <-- Use env var in production
 app.config['MAIL_DEFAULT_SENDER'] = ('Screen Recorder', 'sikandarimran01@gmail.com')
 
 mail = Mail(app)
